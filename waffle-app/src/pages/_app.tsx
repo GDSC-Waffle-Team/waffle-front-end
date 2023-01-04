9d169feada31dac5bcda49f05651b53b5f93c9ee
khj0426 <706shin1728@naver.com>
2023-01-05 00:57:49 +0900

    style:공백제거

--- waffle-app/src/pages/_app.tsx
+++ waffle-app/src/pages/_app.tsx
@@ -1,10 +1,5 @@
 import type { AppProps } from 'next/app';
-import GlobalStyle from '../GlobalStyle';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }

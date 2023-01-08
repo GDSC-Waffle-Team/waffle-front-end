188d4079d5d456216531d1300c645c4ccc3771af
Kim0426 <706shin1728@naver.com>
2023-01-08 23:56:45 +0900

    :feat:getapi testing

--- waffle-app/next.config.js
+++ waffle-app/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/api/main',
+        destination: `http://waffle2.duckdns.org/main`,
+      },
+      {
+        source: '/api/member',
+        destination: `http://waffle2.duckdns.org/member`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

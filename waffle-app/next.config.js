0489d4111edf003ee5441eeb7e2c5b82c8762765
Kim0426 <706shin1728@naver.com>
2023-01-13 00:39:56 +0900

    Update next.config.js

--- waffle-app/next.config.js
+++ waffle-app/next.config.js
@@ -7,10 +7,7 @@ const nextConfig = {
   },
   async rewrites() {
     return [
-      {
-        source: '/api/main',
-        destination: `http://waffle2.duckdns.org/main`,
-      },
+    
       {
         source: '/api/member',
         destination: `http://waffle2.duckdns.org/member`,
